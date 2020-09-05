53d9f2ca99a87b443f5d4ed6b13ee2d7249df790
christiano39 <carneson39@gmail.com>
2020-09-05 13:37:39 -0600

    style weather cards

--- weather-app/src/components/WeatherCard.js
+++ weather-app/src/components/WeatherCard.js
@@ -22,64 +22,77 @@ const WeatherCard = (props) => {
           {weather.name}, {weather.sys.country}
         </h2>
       </CardContent>
-      <CardContent>{weather.weather[0].main}</CardContent>
-      <img
-        src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-        alt={weather.weather[0].main}
-      />
-      {tempUnit === "f" ? (
-        <CardContent>
-          {k_to_f(weather.main.temp)} <span className="f temp-unit">°F</span> |{" "}
-          <span onClick={swapTemp} className="c temp-unit clickable">
-            °C
-          </span>
-        </CardContent>
-      ) : (
-        <CardContent>
-          {k_to_c(weather.main.temp)}{" "}
-          <span onClick={swapTemp} className="f temp-unit clickable">
-            °F
-          </span>{" "}
-          |{" "}
-          <span onClick={swapTemp} className="c temp-unit">
-            °C
-          </span>
-        </CardContent>
-      )}
-      {tempUnit === "f" ? (
-        <CardContent>
-          Wind: {mpsToMph(weather.wind.speed)} mph{" "}
-          <i
-            className="fas fa-arrow-up"
-            style={{ transform: `rotate(${weather.wind.deg}deg)` }}
-          ></i>
-        </CardContent>
-      ) : (
-        <CardContent>
-          Wind: {Math.round(weather.wind.speed)} m/s{" "}
-          <i
-            className="fas fa-arrow-up"
-            style={{ transform: `rotate(${weather.wind.deg}deg)` }}
-          ></i>
-        </CardContent>
-      )}
-      <CardContent>
-        Feels like{" "}
-        {tempUnit === "f"
-          ? `${k_to_f(weather.main.feels_like)} °F`
-          : `${k_to_c(weather.main.feels_like)} °C`}
+      <CardContent className="weather-type">
+        {weather.weather[0].main}
       </CardContent>
-      <CardContent>
-        High:{" "}
-        {tempUnit === "f"
-          ? `${k_to_f(weather.main.temp_max)} °F`
-          : `${k_to_c(weather.main.temp_max)} °C`}{" "}
-        Low:{" "}
-        {tempUnit === "f"
-          ? `${k_to_f(weather.main.temp_min)} °F`
-          : `${k_to_c(weather.main.temp_min)} °C`}
-      </CardContent>
-      <CardContent>Humidity: {weather.main.humidity}%</CardContent>
+      <div className="main-info">
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt={weather.weather[0].main}
+        />
+        {tempUnit === "f" ? (
+          <CardContent className="current-temp">
+            {k_to_f(weather.main.temp)} <span className="f temp-unit">°F</span>{" "}
+            |{" "}
+            <span onClick={swapTemp} className="c temp-unit clickable">
+              °C
+            </span>
+          </CardContent>
+        ) : (
+          <CardContent className="current-temp">
+            {k_to_c(weather.main.temp)}{" "}
+            <span onClick={swapTemp} className="f temp-unit clickable">
+              °F
+            </span>{" "}
+            |{" "}
+            <span onClick={swapTemp} className="c temp-unit">
+              °C
+            </span>
+          </CardContent>
+        )}
+      </div>
+      <div className="lower">
+        <div className="left">
+          <CardContent>
+            Feels like{" "}
+            {tempUnit === "f"
+              ? `${k_to_f(weather.main.feels_like)} °F`
+              : `${k_to_c(weather.main.feels_like)} °C`}
+          </CardContent>
+          <CardContent>
+            High:{" "}
+            {tempUnit === "f"
+              ? `${k_to_f(weather.main.temp_max)} °F`
+              : `${k_to_c(weather.main.temp_max)} °C`}{" "}
+          </CardContent>
+          <CardContent>
+            Low:{" "}
+            {tempUnit === "f"
+              ? `${k_to_f(weather.main.temp_min)} °F`
+              : `${k_to_c(weather.main.temp_min)} °C`}
+          </CardContent>
+        </div>
+        <div className="right">
+          {tempUnit === "f" ? (
+            <CardContent>
+              Wind: {mpsToMph(weather.wind.speed)} mph{" "}
+              <i
+                className="fas fa-arrow-up"
+                style={{ transform: `rotate(${weather.wind.deg}deg)` }}
+              ></i>
+            </CardContent>
+          ) : (
+            <CardContent>
+              Wind: {Math.round(weather.wind.speed)} m/s{" "}
+              <i
+                className="fas fa-arrow-up"
+                style={{ transform: `rotate(${weather.wind.deg}deg)` }}
+              ></i>
+            </CardContent>
+          )}
+          <CardContent>Humidity: {weather.main.humidity}%</CardContent>
+        </div>
+      </div>
     </Card>
   );
 };

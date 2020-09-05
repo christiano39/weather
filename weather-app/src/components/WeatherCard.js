7ea0991855b2d92c5ff178edb4899bfd79829aab
christiano39 <carneson39@gmail.com>
2020-09-05 16:01:34 -0600

    change weather imgs to use https

--- weather-app/src/components/WeatherCard.js
+++ weather-app/src/components/WeatherCard.js
@@ -27,7 +27,7 @@ const WeatherCard = (props) => {
       </CardContent>
       <div className="main-info">
         <img
-          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
           alt={weather.weather[0].main}
         />
         {tempUnit === "f" ? (
